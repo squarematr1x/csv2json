139bd90954844dcd262b8ecf3c342597d50a6cf4
squarematr1x <sasu.pirttipera@protonmail.com>
2022-09-28 16:35:08 +0300

    Add function declaration for tests

---
+++ test/test_csv2json_.py
@@ -0,0 +1,53 @@
+import unittest
+
+import json
+
+from csv2json import csv2json
+
+
+class TestCsv2Json(unittest.TestCase):
+    def test_conversion_semicolon_delimiter(self):
+        pass
+
+    def test_conversion_comma_delimiter(self):
+        pass
+
+    def test_conversion_include_individual_rows(self):
+        pass
+
+    def test_conversion_include_range_rows(self):
+        pass
+
+    def test_conversion_include_individual_and_range_rows(self):
+        pass
+
+    def test_conversion_exclude_individual_rows(self):
+        pass
+
+    def test_conversion_exclude_range_rows(self):
+        pass
+
+    def test_conversion_exclude_individual_and_range_rows(self):
+        pass
+
+    def test_conversion_tail(self):
+        pass
+
+    def test_conversion_head(self):
+        pass
+
+    def test_coversion_include_columns(self):
+        pass
+
+    def test_conversion_exclude_columns(self):
+        pass
+
+    def test_conversion_multiple_options(self):
+        pass
+
+    def test_conversion_empty(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
